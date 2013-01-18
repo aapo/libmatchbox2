aapo/libmatchbox2

commit 60a18963840615aca7da747bbacd2b9935a803d0

Cursor is not invisible. Stage cursor to be shown.
Signed-off-by: Joni Lapilainen <joni.lapilainen@gmail.com>

matchbox/comp-mgr/mb-wm-comp-mgr-clutter.c
@@ -663,7 +663,7 @@ mb_wm_comp_mgr_clutter_init (MBWMObject *obj, va_list vap)
   clutter_actor_show (desktop);
   clutter_container_add_actor (CLUTTER_CONTAINER (arena), desktop);
   priv->desktops = mb_wm_util_list_append (priv->desktops, desktop);
-
+  clutter_stage_show_cursor (CLUTTER_STAGE (desktop));
   return 1;
 }
 

matchbox/core/mb-window-manager.c
@@ -1858,7 +1858,7 @@ mb_window_manager_init (MBWMObject *this, va_list vap)
 
   mb_wm_keys_init(wm);
 
-  /* set the cursor invisible */
+  /* set the cursor invisible
   {
     Pixmap pix = XCreatePixmap (wm->xdpy, wm->root_win->xwindow, 1, 1, 1);
     XColor col;
@@ -1868,7 +1868,7 @@ mb_window_manager_init (MBWMObject *this, va_list vap)
     blank_curs = XCreatePixmapCursor (wm->xdpy, pix, pix, &col, &col, 1, 1);
     XFreePixmap (wm->xdpy, pix);
     XDefineCursor(wm->xdpy, wm->root_win->xwindow, blank_curs);
-  }
+  }*/
 
   return 1;
 }

commit 49390098af8c7ce52949f80b60f9485e21951cf6

Fix segfault introduced by commit 60a18963840615aca7da747bbacd2b9935a803d0
Signed-off-by: Joni Lapilainen <joni.lapilainen@gmail.com>

matchbox/comp-mgr/mb-wm-comp-mgr-clutter.c
@@ -663,7 +663,7 @@ mb_wm_comp_mgr_clutter_init (MBWMObject *obj, va_list vap)
   clutter_actor_show (desktop);
   clutter_container_add_actor (CLUTTER_CONTAINER (arena), desktop);
   priv->desktops = mb_wm_util_list_append (priv->desktops, desktop);
-  clutter_stage_show_cursor (CLUTTER_STAGE (desktop));
+  clutter_stage_show_cursor (CLUTTER_STAGE (clutter_actor_get_stage(desktop)));
   return 1;
 }
 